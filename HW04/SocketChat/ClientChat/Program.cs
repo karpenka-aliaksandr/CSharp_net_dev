4c8ef048e3d8ed5f96ed0fb0860f8da94216f108
Aliaksandr Karpenka <kasasha@mail.ru>
2024-04-10 16:32:10 +0300

    Send others

---
+++ HW04/SocketChat/ClientChat/Program.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using SocketChat.Common.Entities;
+namespace ClientChat
+{
+    internal class Program
+    {
+        static HubConnection HubConnection;
+        async static Task Main(string[] args)
+        {
+            Console.Write("Введите ваше имя: ");
+            string name = Console.ReadLine();
+            HubConnection = new HubConnectionBuilder()
+                .WithUrl("https://localhost:7217/chat")
+                .Build();
+            HubConnection.On<SignalRMessage>("Send", signalRMessage => Print($"{signalRMessage.FromUser}: {signalRMessage.Message}"));
+            await HubConnection.StartAsync();
+            bool isRun = true;
+            while (isRun)
+            {
+                var message = Console.ReadLine();
+                SignalRMessage signalRMessage = new SignalRMessage();
+                signalRMessage.Message = message;
+                signalRMessage.FromUser = name;
+                if (message != "exit")
+                {
+                    await HubConnection.SendAsync("Send", signalRMessage);
+                }
+                else
+                {
+                    isRun = false;
+                }
+
+            }
+        }
+        static void Print(string message)
+        {
+            var ct = Console.CursorTop;
+            var cl = Console.CursorLeft;
+            Console.MoveBufferArea(0,ct,cl,1,0,ct+1);
+            Console.CursorLeft = 0;
+            Console.CursorTop = ct;
+            Console.WriteLine(message);
+            //Console.CursorTop = ct+1;
+            Console.CursorLeft = cl;
+
+     }
+    }
+}
---
+++ HW04/SocketChat/SocketChat.BLL.Logic/ChatHub.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using SocketChat.Common.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocketChat.BLL.Logic
+{
+    public class ChatHub : Hub
+    {
+        public async Task Send(SignalRMessage signalRMessage)
+        {
+            await Clients.Others.SendAsync("Send", signalRMessage);
+        }
+
+        public async Task SendToUser(SignalRMessage signalRMessage)
+        {
+            var client = Clients.Client(signalRMessage.ConnectionId);
+            await client.SendAsync($"message: {signalRMessage.Message}. From user: {signalRMessage.FromUser}");
+        }
+
+        
+
+    }
+}
